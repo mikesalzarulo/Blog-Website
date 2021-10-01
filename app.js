8ac3ea310b481d24c82bd46257b5d551f8a89009
Michael Salzarulo <michaelasalzarulo@gmail.com>
2021-10-01 14:29:22 -0400

    adding navbar routes in ejs template through node and express server side.

--- app.js
+++ app.js
@@ -16,18 +16,16 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 app.get("/", function(req, res){
-  res.render("home");
+  res.render("home", {homeStartingContent: homeStartingContent});
 });
 
+app.get("/about", function(req, res){
+  res.render("about", {aboutContent : aboutContent});
+});
 
-
-
-
-
-
-
-
-
+app.get("/contact", function(req, res){
+  res.render("contact", {contactContent: contactContent});
+});
 
 
 app.listen(3000, function() {
